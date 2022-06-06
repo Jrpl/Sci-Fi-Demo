1020e56b1b3322f71e31fd31bf328f0fdcbc4c3d
Justin Pfeifler <jrpl101998@gmail.com>
2022-06-06 01:40:30 -0500

    Add player movement

---
+++ Assets/Game/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController _controller;
+    [SerializeField]
+    private float _speed = 3.5f;
+    [SerializeField]
+    private float _gravity = 9.81f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        if (!_controller)
+        {
+            Debug.LogError("Failed to get component Character Controller on Game Object: Player");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalcMovement();
+    }
+
+    void CalcMovement()
+    {
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(xInput, 0, zInput);
+        Vector3 velocity = direction * _speed;
+        velocity.y -= _gravity;
+        _controller.Move(velocity * Time.deltaTime);
+    }
+}
