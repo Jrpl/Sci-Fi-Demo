1648744a65718f7137fbb5e5b5a7c962f8b29884
Justin Pfeifler <jrpl101998@gmail.com>
2022-07-18 03:32:43 -0500

    Add shop script

---
+++ Assets/Game/Scripts/SharkShop.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SharkShop : MonoBehaviour
+{
+    // Sound
+    [SerializeField]
+    private AudioClip _buySound;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Player _player = other.gameObject.GetComponent<Player>();
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (_player._hasCoin)
+                {
+                    _player.UpdateHasCoin(false);
+                    AudioSource.PlayClipAtPoint(_buySound, transform.position, 1f);
+                }
+            }
+        }
+    }
+}
